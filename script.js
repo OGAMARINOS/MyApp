52b66934efb09223e36c7730bb9d6c06c486c908
オガサワラ <aogasawara@dl-techx-xedge.net>
2024-11-06 17:15:49 +0900

    資源のデプロイ

--- script.js
+++ script.js
@@ -1,23 +1,49 @@
 const app = new Vue({
-  el: '#app', // Vueが管理する一番外側のDOM要素
-  vuetify: new Vuetify(),
-  data: {
-    // Vue内部で使いたい変数は全てこの中に定義する
-    task: '',
-    todoList: [], // これは配列
-  },
-  methods: {
-    // 関数はここ
-    addTask: function() {
-      console.log('次のタスクが追加されました：', this.task);
-      // 配列の先頭に現在のタスク内容を追加する（最後尾の場合はpush）
-      this.todoList.unshift(this.task);
-      console.log('現在のToDo一覧：', this.todoList);
+    el: '#app', // Vueが管理する一番外側のDOM要素
+    data: {
+      // Vue内部で使いたい変数は全てこの中に定義する
+      fontSize:'50px',
+      isActive: false,
+      isDisplay: false,
+      signal:'red',
+      members: ['本田','香川','長友'],
+      test: 'あああ <br> いいい',
+      book: {
+        title: 'タイトル',
+        author: '著者名',
+        url: 'http://google.com'
+        },
+      books: [
+        {
+          id:1,
+          title: 'タイトル1',
+          author: '著者名1',
+          url: 'http://google.com'    
+        },
+        {
+          id:2,
+          title: 'タイトル2',
+          author: '著者名2',
+          url: 'http://google.com'       
+        },
+        {
+        id:3,
+          title: 'タイトル3',
+          author: '著者名3',
+          url: 'http://google.com'       
+        },
+      ]
+      
+
     },
-    // 以下を追加、関数名はなんでもよい
-    clearAll: function() {
-      this.todoList = [];
-      console.log('全てのToDoが消去されました');
-    },
-  },
+    methods:{
+      btnClicked(){
+        console.log('Vue クリックされた');
+      }
+    }
+    
 });
+
+function btnClicked(){
+  console.log('JS クリックされた');
+}
